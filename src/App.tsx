a8bc1709919f8df439cc0ba3d5e3be0fd4656b30
yiyangzhongbo-commits <yiyangzhongbo-commits@users.noreply.github.com>
2025-09-16 03:51:19 +0900

    最新サイト用 App.tsx に更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-﻿import React from 'react';
-
-function App() {
+﻿function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">中波の最新サイト 🌟</h1>
